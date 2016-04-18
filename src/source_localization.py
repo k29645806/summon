c0e0f5b9c8ac64363dc3f6c19460bae8c4069b64
Kevin Chou <k29645806@gmail.com>
2016-04-18 10:47:52 +0800

    start

---
+++ src/source_localization.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+'''
+Created on November 20, 2010
+
+@author: Dr. Rainer Hessmer
+'''
+import threading
+import serial
+from cStringIO import StringIO
+from std_msgs.msg import String
+import time
+import rospy
+
+class SerialDataGateway(object):
+	'''
+	Helper class for receiving lines from a serial port
+	'''
+	def __init__(self, port="/dev/ttyUSB1", baudrate=115200):
+		'''
+		Initializes the receiver class.
+		port: The serial port to listen to.
+		receivedLineHandler: The function to call when a line was received.
+		'''
+		self._Port = port
+		self._Baudrate = baudrate
+		self._KeepRunning = False
+		self.serialReadData = ""
+		self.f_readRenew = False
+
+	def Start(self):
+		try:
+			self._Serial = serial.Serial(port=self._Port, baudrate=self._Baudrate, timeout=0)
+		except:
+			rospy.loginfo("SERIAL PORT Start Error")
+			raise
+		self._KeepRunning = True
+		self._ReceiverThread = threading.Thread(target=self.Listen)
+		self._ReceiverThread.setDaemon(True)
+		self._ReceiverThread.start()
+
+	def Stop(self):
+		rospy.loginfo("Stopping serial gateway")
+		self._KeepRunning = False
+		time.sleep(.1)
+		try:
+			self._Serial.close()
+		except:
+			rospy.loginfo("SERIAL PORT Stop Error")
+			raise
+
+	def Listen(self):
+		stringIO = StringIO()
+		self._Serial.flushInput()
+		start_time = time.time()
+		while self._KeepRunning:
+			try:
+				data = self._Serial.read()
+			except:
+				rospy.loginfo("SERIAL PORT Listen Error")
+				raise
+			if (data == '\n'):
+				self.serialReadData = stringIO.getvalue()
+				self.f_readRenew = True
+				print self.serialReadData
+				stringIO.close()
+				stringIO = StringIO()
+			else:
+				stringIO.write(data)
+
+	def Write(self, data):
+		#AttributeError: 'SerialDataGateway' object has no attribute '_Serial'
+		try:
+			# print("serial write : " + data)
+			self._Serial.write(data)
+		except AttributeError:
+			rospy.loginfo("SERIAL PORT Write Error")
+			raise
+
+	def handleCommand(self, data):
+		# print data.data
+		self.Write(data.data)
+
+	def getSerialReadData(self):
+		return self.serialReadData
+
+	def getFlagReadRenew(self):
+		return self.f_readRenew
+
+	def setFlagReadRenew(self, flag):
+		self.f_readRenew = flag
+
+if __name__ == '__main__':
+	dataReceiver = SerialDataGateway("/dev/ttyACM1", 2400)
+	rospy.init_node('serial_node')
+	serialPublisher = rospy.Publisher('serial/read', String, queue_size=10)
+	#rospy.Subscriber("serial/write", String, dataReceiver.handleCommand)  # Is this line or the below bad redundancy?
+	dataReceiver.Start()
+	rate = rospy.Rate(100)
+	try:
+		while not rospy.is_shutdown():
+			if (dataReceiver.getFlagReadRenew()):
+				serialPublisher.publish(String(dataReceiver.getSerialReadData()))
+				dataReceiver.setFlagReadRenew(False)
+			else:
+				serialPublisher.publish(String("-1"))
+			rate.sleep()
+	finally:
+		dataReceiver.Stop()
---
+++ src/summon_no_filter.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+import rospy
+import time
+from math import degrees
+from numpy import median, std, mean
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point32,Twist
+from std_msgs.msg import String
+from kobuki_msgs.msg import BumperEvent,Led
+import tf
+from tf.transformations import euler_from_quaternion
+
+class Kobuki(object):
+    fwd_speed, rotate_speed = 0.0, 0.0
+    x, y, heading = 0.0, 0.0, 0.0  ## heading unit: degrees
+    x_record, y_record, heading_record, final_voice_angle_record = 0.0, 0.0, 0.0, 0.0
+    voice_angle = -1   ## voice angle unit: degrees
+    voiceID = 0
+    heading_desire, distance_desire = 0.0, 2.0
+    state = 'idle'
+    angle_remain, distance_remain = 0.0, 0.0
+
+    def __init__(self, fwd_speed=100, rotate_speed=100):
+        self.default_fwd_speed = fwd_speed
+        self.default_rotate_speed = rotate_speed
+    def subscribe(self):
+        rospy.Subscriber('/odom',Odometry, self.callback_odom)  # from robot_setup.py
+        rospy.Subscriber("/serial/read", String, self.callback_voice_angle) # from sound_localization.py
+        rospy.Subscriber('/output',String, self.callback_voiceID)  # from recognizer.py
+    def callback_odom(self, data):
+        self.x = data.pose.pose.position.x
+        self.y = data.pose.pose.position.y
+        q1 = data.pose.pose.orientation.x
+        q2 = data.pose.pose.orientation.y
+        q3 = data.pose.pose.orientation.z
+        q4 = data.pose.pose.orientation.w
+        q = (q1, q2, q3, q4)
+        e = euler_from_quaternion(q)
+        yaw = degrees(e[2])
+        self.heading = self.to_positive_angle(yaw)
+    def callback_voice_angle(self, data):
+        self.voice_angle = -1 if int(data.data)<0 else -int(data.data) + 360  # cvt from CW to CCW
+    def callback_voiceID(self, data):
+        self.voiceID = int(data.data)
+        time.sleep(1)
+        self.voiceID = 0
+    def idle(self, final_voice_angle, exist=False):
+        self.fwd_speed = 0.0
+        self.rotate_speed = 0.0
+        self.x_record = self.x
+        self.y_record = self.y
+        self.heading_record = self.heading
+        self.final_voice_angle_record = final_voice_angle
+        self.state = 'rotate' if (exist and self.voiceID) else 'idle'
+    def rotate(self):
+        self.fwd_speed = 0.0
+        self.rotate_speed = self.default_rotate_speed
+        self.heading_desire = self.heading_record + self.final_voice_angle_record
+        self.angle_remain = self.find_smallest_rotation_angle(self.heading_desire, self.heading)
+        self.rotate_speed = self.default_rotate_speed if self.angle_remain > 0 else -self.default_rotate_speed
+    def rotate_state_check(self):
+        if abs(self.angle_remain) <= 5.0:
+            rospy.loginfo('Reached desired heading')
+            self.state = 'forward'
+        else:
+            self.state = 'rotate'
+    def forward(self):
+        self.fwd_speed = self.default_fwd_speed
+        self.rotate_speed = 0.0
+        dx = self.x - self.x_record
+        dy = self.y - self.y_record
+        self.distance_remain = (dx**2 + dy**2)**0.5
+    def forward_state_check(self):
+        if self.distance_remain >= self.distance_desire or self.bumper:
+            rospy.loginfo('Reached')
+            self.state = 'idle'
+        else:
+            self.state = 'forward'
+## ----------------------------------------------------------------------------
+    def print_info(self, possibility):
+        #print 'state:%s x:%s y:%s heading:%s'%(self.state, self.x, self.y, self.heading)
+        #print 'state:%s possibility:%s'% (self.state, possibility)
+        #print 'voice_angle:%s angle_remain:%s distance_remain:%s'\
+        #%(self.final_voice_angle_record, self.angle_remain, self.distance_remain)
+        #print self.voiceID
+    def pub_cmd(self):
+        pub = rospy.Publisher('/mobile_base/commands/velocity' ,Twist, queue_size=10)
+        msg = Twist()
+        msg.linear.x = self.fwd_speed
+        msg.angular.z = self.rotate_speed
+        pub.publish(msg)
+    def find_smallest_rotation_angle(self, heading_desire, heading_now):
+        heading_desire = heading_desire % 360
+        heading_now = heading_now % 360
+        a = heading_desire - heading_now
+        b = (heading_desire + 360) - heading_now
+        c = heading_desire - (heading_now + 360)
+        lst = [a, b, c]
+        lst_abs = [abs(x) for x in lst]
+        index = lst_abs.index(min(lst_abs))
+        return lst[index]
+    def to_positive_angle(self, theta):
+        while True:
+            if theta < 0:
+                theta += 360
+            if theta > 0:
+                theta = theta % 360
+                return theta
+                break
+
+class Voice(object):
+    def __init__(self, freq_mcu=30.0, freq_main=100.0 , voice_duration=1):
+        self.freq_mcu = freq_mcu
+        self.freq_main = freq_main
+        self.max_percentage = freq_mcu / freq_main
+        self.check_length = int(freq_main * voice_duration)
+        self.voice_angle_lst = [-1] * self.check_length
+
+    def siso(self, lst, value):
+        lst.insert(0, value)
+        lst.pop()
+        return lst
+
+    def listen(self, voice_angle):
+        self.voice_angle_lst = self.siso(self.voice_angle_lst, voice_angle)
+        signal_numbers = self.check_length - self.voice_angle_lst.count(-1) ##    Signal in a moving array
+
+        if signal_numbers:
+            heard_lst = [x for x in self.voice_angle_lst if x >=0]
+            self.std = std(heard_lst) / mean(heard_lst)
+            self.percentage = float(signal_numbers) / self.check_length
+            self.possibility = self.percentage / self.max_percentage
+        else:
+            self.possibility = 0.0
+
+        if self.possibility > 0.7:
+            self.exist = True
+            self.final_voice_angle = median(heard_lst)
+            self.temp = self.voice_angle_lst
+            self.voice_angle_lst = [-1] * self.check_length   ## Reset the moving array
+        else:
+            self.exist = False
+            self.final_voice_angle = 0
+
+
+rospy.init_node("summon")
+rate = rospy.Rate(100)
+ear = Voice()
+kbk = Kobuki()
+kbk.subscribe()
+
+if __name__ == '__main__':
+    while not rospy.is_shutdown():
+        ear.listen(kbk.voice_angle)
+        kbk.print_info(ear.possibility)
+        if kbk.state == 'idle':
+            kbk.idle(ear.final_voice_angle, ear.exist)
+        elif kbk.state == 'rotate':
+            kbk.rotate()
+            kbk.rotate_state_check()
+        elif kbk.state == 'forward':
+            kbk.forward()
+            kbk.forward_state_check()
+
+        kbk.pub_cmd()
+        rate.sleep()
